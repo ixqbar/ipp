ae0cc6ab621a7e9f924ba11af9ba3755f965b9ea
xingqiba <ixqbar@gmail.com>
2018-12-29 14:00:13 +0800

    optimize

--- src/ipp/const.go
+++ src/ipp/const.go
@@ -1,3 +1,3 @@
 package ipp
 
-const VERSION = "0.0.1"
+const VERSION = "0.0.2"
--- src/ipp/server.go
+++ src/ipp/server.go
@@ -17,40 +17,37 @@ type TServer struct {
 
 func NewTServer() *TServer {
 	return &TServer{
-		stopChannel:make(chan bool, 0),
+		stopChannel: make(chan bool),
 	}
 }
 
-func (obj *TServer) RunServer() {
+func (obj *TServer) RunServer() error {
 	raddr, err := net.ResolveUDPAddr("udp", ":9595")
 	if err != nil {
 		Logger.Print(err)
-		return
+		return err
 	}
 
 	conn, err := net.ListenUDP("udp", raddr)
 	if err != nil {
 		Logger.Print(err)
-		return
+		return err
 	}
 
 	defer conn.Close()
 
 	go func() {
-		for {
-			select {
-			case <-obj.stopChannel:
-				Logger.Print("server mode catch stop signal")
-				conn.SetDeadline(time.Now().Add(time.Second * 3))
-				return
-			}
+		select {
+		case <-obj.stopChannel:
+			Logger.Print("server mode catch stop signal")
+			conn.SetDeadline(time.Now().Add(time.Second * 3))
 		}
 	}()
 
 	data := make([]byte, 1024)
 
 	for {
-		n,c,err := conn.ReadFromUDP(data)
+		n, c, err := conn.ReadFromUDP(data)
 		if err != nil {
 			Logger.Print(err)
 			if nerr, ok := err.(net.Error); ok && nerr.Timeout() {
@@ -65,19 +62,20 @@ func (obj *TServer) RunServer() {
 	}
 
 	Logger.Print("server exit server mode")
+	return nil
 }
 
-func (obj *TServer) RunClient()  {
+func (obj *TServer) RunClient() error {
 	raddr, err := net.ResolveUDPAddr("udp", "255.255.255.255:9595")
 	if err != nil {
 		Logger.Print(err)
-		return
+		return err
 	}
 
 	conn, err := net.DialUDP("udp", nil, raddr)
 	if err != nil {
 		Logger.Print(err)
-		return
+		return err
 	}
 
 	defer conn.Close()
@@ -90,10 +88,10 @@ func (obj *TServer) RunClient()  {
 E:
 	for {
 		select {
-		case <- obj.stopChannel:
+		case <-obj.stopChannel:
 			Logger.Print("client mode catch stop signal")
 			break E
-		case <- interval.C:
+		case <-interval.C:
 			var currentMachineIps string = strings.Join(GetCurrentMachineIps(), "|")
 			Logger.Printf("client send packet %s", currentMachineIps)
 			conn.Write([]byte(currentMachineIps))
@@ -101,29 +99,46 @@ E:
 	}
 
 	Logger.Print("server exit client mode")
+	return nil
 }
 
 func (obj *TServer) Stop() {
 	obj.stopChannel <- true
 }
 
-func Run(runAsServer bool)  {
-	sigs := make(chan os.Signal)
-	signal.Notify(sigs, syscall.SIGTERM, syscall.SIGINT)
+func Run(runAsServer bool) {
+	errorChan := make(chan bool)
+	signalChan := make(chan os.Signal)
+
+	signal.Notify(signalChan, syscall.SIGTERM, syscall.SIGINT)
 
 	server := NewTServer()
 
 	go func() {
-		<-sigs
-		Logger.Print("server catch exit signal")
-		server.Stop()
+		Logger.Printf("signal goroutine running")
+
+		select {
+		case <-signalChan:
+			Logger.Print("signal goroutine catch exit signal")
+			server.Stop()
+		case <-errorChan:
+			Logger.Print("signal goroutine catch error signal")
+		}
+
+		Logger.Printf("signal goroutine exit")
 	}()
 
 	Logger.Print("server start running")
 	if runAsServer {
-		server.RunServer()
+		err := server.RunServer()
+		if err != nil {
+			errorChan <- true
+		}
 	} else {
-		server.RunClient()
+		err := server.RunClient()
+		if err != nil {
+			errorChan <- true
+		}
 	}
 
 	Logger.Print("server stop running")
